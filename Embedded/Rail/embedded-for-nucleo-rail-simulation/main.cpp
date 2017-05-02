57e0a1a6c70be7c12149b5549a85d9a029076dc7
ukrishva <ukrishva@gmail.com>
2017-05-02 22:08:01 +0700

    Serial connection with python and traffic simulation

---
+++ Embedded/Rail/embedded-for-nucleo-rail-simulation/main.cpp
@@ -0,0 +1,65 @@
+/**
+    ICT710 Project Assignment  
+    main.cpp
+    Purpose: Turn on/off Train lamp for Rails simulation.
+
+    @author Ukrish Vanichrujee
+    @version 1.0 4/26/2017
+*/
+#include "mbed.h"
+
+DigitalOut led(LED1);// test led
+DigitalOut led_red(D15);// red
+DigitalOut led_yellow1(D11);// yellow1
+DigitalOut led_yellow2(D14);// yellow2
+DigitalOut led_green(D13);// green
+RawSerial pc(USBTX, USBRX);
+
+void lamp(char c){
+    
+    switch(c){
+    
+        case 'p':
+            led_yellow2 = 1;
+            led_yellow1 = 0;
+            led_red = 0;
+            led_green = 0;
+            break;
+        case 'y':
+            led_yellow2 = 0;
+            led_yellow1 = 1;
+            led_red = 0;
+            led_green = 0;
+            break;
+        case 'r':
+            led_yellow2 = 0;
+            led_yellow1 = 0;
+            led_red = 1;
+            led_green = 0;
+            break;
+        case 'g':
+            led_yellow2 = 0;
+            led_yellow1 = 0;
+            led_red = 0;
+            led_green = 1;
+            break;
+        case 'c':
+            led_yellow2 = 0;
+            led_yellow1 = 0;
+            led_red = 0;
+            led_green = 0;
+            break;
+    }
+    
+}
+int main() {
+    while(1){
+        
+        // Read Data from Python via serial 
+        if (pc.readable()) {
+            char ch = pc.getc();
+            lamp(ch);
+        }
+        
+    }
+}
---
+++ Embedded/Rail/python-for-serial-connection/rail_siit.py
@@ -0,0 +1,97 @@
+'''
+    This script is used to write characters to NUCLEO F401RE via serial port.
+'''
+import serial
+import time
+import json
+import urllib2
+import io
+
+__author__ = "Ukrish Vanichrujee"
+__copyright__ = "Copyright 2017, ICTES Train Simulation Project"
+__credits__ = ["Paradorn Pimporn"]
+__license__ = "GPL"
+__version__ = "1.0.0"
+__email__ = "ukrishva@gmail.com"
+
+# For SIIT
+# Start Serial connection
+ser = serial.Serial()
+ser.baudrate = 9600
+ser.port = 'COM4'# pc port
+id = 1 #SIIT's ID is = 1
+ip = "sw-final-project.appspot.com"
+GREEN = 1000
+YELLOW1 = 750
+YELLOW2 = 200
+#this line can be written as "192.168.1.10:8080"
+try:
+    ser.open()
+    i = 0
+    while (1):
+        # HTTP request
+        try:
+            url = "http://%s/getlocation?train_no=%d" %(ip,id)  # Local Addr
+            resp = urllib2.urlopen(url)
+            result = json.load(resp)
+            distance = result['distance_track']
+            print("Distance:%d" % (result['distance_track']))
+            print("Next station:%s" % (result['next_station']))
+            light = ''
+            if distance > GREEN:
+                light = 'green'
+                ser.write('g')
+            elif (distance < GREEN) and (distance > YELLOW1):
+                light = 'yellow1'
+                ser.write('y')
+            elif (distance < YELLOW1) and (distance > YELLOW2):
+                light = 'yellow2'
+                ser.write('p')
+            elif (distance < YELLOW2) and (distance > 0):
+                ser.write('r')
+                light = 'red'
+            elif distance == 0:
+                print('Start Blinking')
+                light = 'red'
+                j = 0
+                ser.write('r')
+                url = "http://%s/insert_traffic?train_no=%d&light=%s" \
+                       % (ip,id, light)
+                resp = urllib2.urlopen(url)
+                time.sleep(10)
+                light = 'blink'
+                url = "http://%s/insert_traffic?train_no=%d&light=%s" \
+                      % (ip, id, light)
+                resp = urllib2.urlopen(url)
+                while j < 15:
+                    ser.write('c')
+                    time.sleep(.5)
+                    ser.write('r')
+                    time.sleep(.5)
+                    j += 1
+                ser.write('g')
+                light = 'green'
+                url = "http://%s/insert_traffic?train_no=%d&light=%s" \
+                      % (ip, id, light)
+                resp = urllib2.urlopen(url)
+                time.sleep(5)
+                print('Train takes off')
+            print("Traffic Light:%s" % (light))
+            url = "http://%s/insert_traffic?train_no=%d&light=%s" \
+                  % (ip, id, light)
+            resp = urllib2.urlopen(url)
+            print('==================')
+        except IOError:
+            print('There is something wrong with serial connection.')
+            break
+        except Exception:
+            print('There is something wrong with http connection.')
+            break
+        time.sleep(2)
+        i += 1
+except IOError:
+    print('There is something wrong with serial connection.')
+finally:
+    ser.close()
+# End Serial connection
+# ser.isOpen()
